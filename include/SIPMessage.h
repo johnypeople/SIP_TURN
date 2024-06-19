b2d7abc038610f0c99e653bda9dd047cb17cb0db
johnypeople <gjoao18@gmail.com>
2024-06-19 18:28:54 -0300

    Add class SIPMessage declaration

---
+++ include/SIPMessage.h
@@ -0,0 +1,21 @@
+#ifndef SIPMESSAGE_H
+#define SIPMESSAGE_H
+
+#include <string>
+
+class SIPMessage {
+    public:
+        SIPMessage(const std::string& type, const std::string& from, const std::string& to);
+        std::string getType() const; 
+        std::string getFrom() const; 
+        std::string getTo() const;
+        std::string getString() const;
+        
+    
+    private:
+        std::string type;
+        std::string from;
+        std::string to;
+};
+
+#endif //SIPMESSAGE_H
